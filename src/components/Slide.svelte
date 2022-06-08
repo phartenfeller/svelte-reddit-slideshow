f0375213d07aa1cd34544eb7b9f67a533a023fbd
phartenfeller <pharti@hotmail.de>
2022-06-08 09:10:50 +0200

    css center and max width for videos

--- src/components/Slide.svelte
+++ src/components/Slide.svelte
@@ -2,6 +2,8 @@
   export let mediaUrl;
   export let mediaExtension;
 
+  console.log({ mediaUrl, mediaExtension });
+
   const muted = false;
 
   const endedHandler = () => {
@@ -11,11 +13,25 @@
 
 <div>
   {#if mediaExtension === '.gifv'}
-    <video id="slide" autoPlay loop {muted} onPlaying={endedHandler}>
+    <video
+      id="slide"
+      class="video-frame"
+      autoPlay
+      loop
+      {muted}
+      onPlaying={endedHandler}
+    >
       <source src={mediaUrl.replace('.gifv', '.mp4')} type="video/mp4" />
     </video>
   {:else if ['.mp4', '.webm'].includes(mediaExtension)}
-    <video id="slide" autoPlay loop {muted} onPlaying={endedHandler}>
+    <video
+      id="slide"
+      class="video-frame"
+      autoPlay
+      loop
+      {muted}
+      onPlaying={endedHandler}
+    >
       <source src={mediaUrl.replace('.mp4', '.webm')} type="video/webm" />
       <source src={mediaUrl} type="video/mp4" />
     </video>
@@ -36,5 +52,10 @@
     background-repeat: no-repeat;
     background-position: center;
     background-size: contain;
+  }
+
+  .video-frame {
+    width: 100vw;
+    height: 100vh;
   }
 </style>
